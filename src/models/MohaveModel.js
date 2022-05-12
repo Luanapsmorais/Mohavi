3496f9457a7991ea39076b86713d983f0f9bc090
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-05-12 09:41:50 -0300

    Update MohaveModel.js

--- src/models/MohaveModel.js
+++ src/models/MohaveModel.js
@@ -1,12 +1,40 @@
-const MohaveModel = {
+const { Alunos } = require('sequelize')
 
 
-    obterinformacoes: () => ([
-        'alunoum',
-        'alunodois',
-        'alunotres'
-    ])
 
-};
 
-module.exports = MohaveModel
+
+module.exports = (sequelize, dataTypes) => {
+    class Alunos extends Model{
+       
+    }
+    Alunos.init({
+        id:{
+            type:dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull:false
+        },
+            nome:{
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+            sobreNome:{
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+        matricula:{
+            type:dataTypes.INTEGER,
+            allowNull:false
+        }
+    }, {
+        sequelize, 
+        tablename: 'alunos',
+        timestamps:false
+    })
+
+    return Alunos
+}
+
+
+
