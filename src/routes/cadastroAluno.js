e74f88639ec9d47bfba15bb551c85607bfcfa63a
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-05-12 09:46:28 -0300

    Update cadastroAluno.js

--- src/routes/cadastroAluno.js
+++ src/routes/cadastroAluno.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../middlewares/multer');
+const alunosContoller = require('../controllers/AlunosController')
 
 
 const MohaveController = require('../controllers/MohaveController')
 router.get('/', MohaveController.index);
 router.get('/cadastro', MohaveController.cadastro);
 router.post('/cadastro', upload.single('servico-img'), MohaveController.cadastro);
-
+router.post('/cadastro-test', alunosContoller.create);
 
 module.exports = router;
