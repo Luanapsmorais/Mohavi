923fe6a8f6bf827e1a6557fdc8a40634b53d7255
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-05-12 09:34:17 -0300

    Create AlunosController.js

---
+++ src/controllers/AlunosController.js
@@ -0,0 +1,15 @@
+const alunosServices = require('../services/alunosServices')
+
+const alunosContoller  = {
+    create: async (req, res) => {
+        const {
+        nome, email, tel, senha
+        } = req.body
+
+        const response = await alunosServices.create(nome, email, tel, senha)
+
+        res.json(response)
+    },
+}
+
+module.exports = alunosContoller;
