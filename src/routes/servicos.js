e367a732acfcddfc53009b77e377b3abdc7c1e88
Solange Minossi <Solange Minossi@gmail.com>
2022-05-28 17:41:11 -0300

    alteração em serviços para aulas

--- src/routes/servicos.js
+++ src/routes/servicos.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const router = express.Router();
-const ServicoController = require('../controllers/ServicoController');
+const AulaController = require('../controllers/AulaController');
 const upload = require('../middlewares/multer');
 const auth = require('../middlewares/auth');
 
