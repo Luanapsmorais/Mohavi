8ad0b60f63823f3e59ecaadb5a7536e5f1f4e6e3
Solange Minossi <Solange Minossi@gmail.com>
2022-05-28 17:41:11 -0300

    alteração em serviços para aulas

--- src/routes/servicos.js
+++ src/routes/servicos.js
@@ -1,21 +1,15 @@
 const express = require ('express');
 const router = express.Router();
-const ServicoController = require('../controllers/ServicoController');
 const validadorCadastroServicos = require('../middlewares/validadorCadastroServicos');
-//const upload = require('../middlewares/multer');
+const AulasController = require('../controllers/AulasController');
 const auth = require('../middlewares/auth');
 const multer = require('../middlewares/multer')
-
-  
-
 
 router.post('/cadastrar', multer.single('servico-img'), validadorCadastroServicos, AulasController.criar);
 router.get('/cadastrar', AulasController.editar);
 
 router.get('/', AulasController.index);
 router.get('/admin', auth, AulasController.admin);
-
-
 
 router.put('/editar/:id', multer.single('servico-img'), validadorCadastroServicos, AulasController.atualizar);
 router.get('/editar/:id', AulasController.editar);
