1f182b39628d50e7a01605bf65a7acf9549a7c47
“AssFerj” <assisjuniorcam@gmail.com>
2022-06-01 11:59:35 -0300

    Atualizacao de controller e router

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -3,7 +3,7 @@ const UsuarioModel = require('../models/UsuarioModel');
 const UsuarioController = {
     form: (req, res) => {
         return res.render('cadastroAluno');
-    },
+    }
 }
 
 module.exports = UsuarioController;
