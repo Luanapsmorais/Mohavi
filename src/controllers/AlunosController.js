3b3c7445ab034a392a4c7a6c080bdf6f9d327e29
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-05 19:08:34 -0300

    Update AlunosController.js

--- src/controllers/AlunosController.js
+++ src/controllers/AlunosController.js
@@ -1,15 +1,9 @@
-const alunosServices = require('../services/alunosServices')
+const AlunoModel = require('../models/AlunoModel');
 
-const alunosContoller  = {
-    create: async (req, res) => {
-        const {
-        nome, email, tel, senha
-        } = req.body
-
-        const response = await alunosServices.create(nome, email, tel, senha)
-
-        res.json(response)
+const AlunosController = {
+    form: (req, res) => {
+        return res.render('cadastroAluno');
     },
 }
 
-module.exports = AlunosContoller;
+module.exports = AlunosController;
