3bb30a157e87db82a67820c9381e2376aae96d68
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-05 18:29:15 -0300

    Update AulaModel.js

--- src/models/AulaModel.js
+++ src/models/AulaModel.js
@@ -1,10 +1,41 @@
-
-
 const { uuid } = require('uuidv4');
 const path = require('path');
 const fs = require('fs');
 
+module.exports=()=>{
+    const Aula = sequelize.define('Aula', {
+        id: {
+            type: dataTypes.INTEGER,
+            PRIMARYkEY: TRUE,
+            autoIncrement: true,
+            allowNull: false
+        }, 
+        cadastro_dt: {
+            type: dataType.DATE,
+            allowNull: false
+        }, 
+        alteracao_dt: {
+            type: dataType.DATE,
+            allowNull: false
+        },
+
+    }, {
+        tableName: 'aulas',
+        timestamp: false
+    });
+    Aula.associate = (models)=>{
+        Aula.hasMany(models.Turno, {
+            foreignKey: 'turnos_id',
+            as:'turnos'
+        })
+    }
+
+    return Aula;
+}
+
+
 module.exports = {
+//Código antigo mantido por segurança
   aula: [
         { 
             id1:  1,
@@ -61,6 +92,8 @@ module.exports = {
         fs.unlink('public' + filePatch, (err) => console.log('err'));
 
         const index = this.aulas.findIndex(aula =>aula.id == id);
+
+
     }
 
 };
